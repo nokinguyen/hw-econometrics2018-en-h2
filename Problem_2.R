5e973c0c8698d460605f87d351ab3a5bef9708c6
nokinguyen <37672610+nokinguyen@users.noreply.github.com>
2018-04-29 23:41:47 +0300

    Update Problem_2.R

--- Problem_2.R
+++ Problem_2.R
@@ -13,6 +13,8 @@ set.seed()
 
 # use the rnorm function to generate values from the standard normal distribution
 # you can compute e^x with the exp function, e.g. e^2 = exp(2).
+mu <- 1
+sigma <- 1.3
 
 ## Draw n * R values at random from the standard normal distribution
 ## Note: you need to define n, R and furthermore mu and sigma.
@@ -23,9 +25,35 @@ x <- exp(mu + sigma * z)
 ## the methods used in the simulation in Problme set 2/Problem 1.
 
 ## a)
+mux <- exp((mu) + sigma^2/2)
+varx <- (exp(sigma^2) - 1) * exp(2*mu + sigma^2)
 
 ## b)
+B <- 5000
+n <- 1000
+z <- rnorm(B * n)
+x <- exp(mu + sigma * z)
+sampleB <- sample(x, size = n*B, replace = TRUE)
 
 ## c)
+samplesData <- data.frame(
+  minutes = sampleB, 
+  r = rep(1:B, each = n)
+)
+
+groupedSamples <- group_by(samplesData, r)
+
+sampleMeansbySample <- summarise(
+  groupedSamples,
+  sampleMean = mean(minutes)
+)
 
 ## d)
+ggplot(data = sampleMeansbySample, aes(x = sampleMean)) + 
+  geom_histogram(bins=50) + geom_vline(xintercept = mean(x), color = 'orange1')
+
+## The distribution resembles a normal distribution. It isn't skewed. It's centered around the ## mean value.
+
+## e)
+mean(sampleMeansbySample$sampleMean)
+sd(sampleMeansbySample$sampleMean)
